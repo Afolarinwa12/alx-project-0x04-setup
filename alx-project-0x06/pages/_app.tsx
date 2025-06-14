88d5fc823fb83cf31ccffaad0a7e979fab799118
Afolarinwa12 <Afolarinwa12@gmail.com>
2025-06-14 22:11:21 +0100

    task 2

---
+++ alx-project-0x06/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { Provider } from 'react-redux'; // Import the Provider component
+
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import store from '@/store/store';
+import Layout from '@/components/layouts/Layout';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+   
+ <Provider store={store}>
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+      
+    </Provider>
+
+  )
+}
